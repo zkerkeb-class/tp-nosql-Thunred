c08fc46bb6f72c0879aa5d47846da10f436564ee
Thunred <nicolas.nd40@gmail.com>
2026-02-16 14:31:41 +0100

    mise a jour cour des fichier

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
+import pokemons from './schemas/pokemons.js';
 
+import './connect.js'
 
 
 const app = express();
 
 app.use(cors()); // Permet les requêtes cross-origin (ex: frontend sur un autre port)
+app.use(express.json());
 
 app.use('/assets', express.static('assets')); // Permet d'accéder aux fichiers dans le dossier "assets" via l'URL /assets/...
 
-app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/api/pokemons', async (req, res) => {
+    // Ici, vous pouvez récupérer les pokémons depuis la base de données
+    const pokemonsList = await pokemons.find(); // Récupère tous les pokémons de la collection
+    res.json(pokemonsList);
+})
 
 
 app.listen(process.env.PORT || 3000, () => {
